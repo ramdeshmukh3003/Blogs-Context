33d522148836190b6855dfa001df1fcf67e72efc
ramdeshmukh <ramdeshmukh3003@gmail.com>
2023-12-30 17:59:59 +0530

    Files Added

---
+++ blogs-context-2-starter/src/components/Blogs.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import BlogDetails from "./BlogDetails";
+export default function Blogs() {
+  const { posts, loading } = useContext(AppContext);
+
+  return (
+    <div className="w-11/12 max-w-[650px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center">
+      {loading ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">Loading</p>
+        </div>
+      ) : posts.length === 0 ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">No Blogs Found !</p>
+        </div>
+      ) : (
+        posts.map((post) => (
+          <BlogDetails key={post.id} post={post} />
+        ))
+      )}
+    </div>
+  );
+}
