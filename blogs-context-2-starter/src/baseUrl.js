a5de1d7edbc56cbaeb0e0f99ba13cb51b0404686
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-02-24 12:01:21 +0530

    Delete blogs-context-2-starter directory

--- blogs-context-2-starter/src/baseUrl.js
+++
@@ -1,1 +0,0 @@
-export const baseUrl = "https://codehelp-apis.vercel.app/api/get-blogs";
--- blogs-context-2-starter/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white">
-      <h1 className="font-bold text-3xl uppercase text-center">
-       Trending Blogs
-      </h1>
-    </header>
-  );
-}
